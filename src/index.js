e9ad527406d2441d02ae290719932493c883031a
Chuabacca <jonathan.chua@dollarshaveclub.com>
2017-03-04 07:49:11 -0800

    Added props to React compoenent.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const color = Math.random() > 0.5 ? 'lavender' : 'fuchsia';
+const App = (props) => {
+  return (
+    <h2 className="text-center">
+      {props.headerMessage}
+    </h2>
+  );
+}; 
+
+App.propTypes = {
+  headerMessage: React.PropTypes.string
+};
 
 ReactDOM.render(
-    <h2 style={{color}}>
-      Hello from React with JSX
-    </h2>,
+    <App headerMessage={"I am a React Component with Props."} />,
     document.getElementById('root')
 );
