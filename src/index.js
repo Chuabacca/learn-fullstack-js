42e850ae3acebe54e074b135956abcd75fffa04b
Chuabacca <jonathan.chua@dollarshaveclub.com>
2017-03-04 06:43:37 -0800

    Using normal HTML in React components via JSX.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const color = Math.random() > 0.5 ? 'lavender' : 'fuchsia';
+
 ReactDOM.render(
-    React.createElement(),
+    <h2 style={{color}}>
+      Hello from React with JSX
+    </h2>,
     document.getElementById('root')
 );
